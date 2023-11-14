446dce7d43636a58bba90170a21ea37c51b83c10
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2023-11-14 22:07:47 +0100

    Support launching boardgen CLI as a module

--- boardgen/__init__.py
+++ boardgen/__init__.py
@@ -1,5 +1,11 @@
 # Copyright (c) Kuba Szczodrzyński 2022-05-11.
 
+import os
+import sys
+
+while os.getcwd() in sys.path:
+    sys.path.remove(os.getcwd())
+
 from . import models, shapes, utils
 from .core import Core
 from .mixins import HasId, HasVars, ParentType
---
+++ boardgen/__main__.py
@@ -0,0 +1,6 @@
+#  Copyright (c) Kuba Szczodrzyński 2023-11-14.
+
+from .cli import cli
+
+if __name__ == "__main__":
+    cli()
